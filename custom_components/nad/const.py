f69dc17836345878868baed552e62706f88f697f
tomkraemer <tom.dummie@web.de>
2026-08-03 11:50:35 +0200

    Kommentare

--- custom_components/nad/const.py
+++ custom_components/nad/const.py
@@ -8,13 +8,13 @@ CONF_TYPE_SERIAL: Final = "RS232"
 CONF_TYPE_TELNET: Final = "Telnet"
 CONF_TYPE_TCP: Final = "TCP"
 
-CONF_SERIAL_PORT: Final = "serial_port"  # for NADReceiver
-CONF_DEFAULT_PORT: Final = 23
+CONF_SERIAL_PORT: Final = "serial_port"     # for NADReceiver
+CONF_DEFAULT_PORT: Final = 23               # 53 klappt bei uns nicht
 
 CONF_MIN_VOLUME: Final = "min_volume"
 CONF_MAX_VOLUME: Final = "max_volume"
-CONF_VOLUME_STEP: Final = "volume_step"  # for NADReceiverTCP
-CONF_SOURCE_DICT: Final = "sources"  # for NADReceiver
+CONF_VOLUME_STEP: Final = "volume_step"     # for NADReceiverTCP
+CONF_SOURCE_DICT: Final = "sources"         # for NADReceiver
 
 CONF_DEFAULT_MIN_VOLUME: Final = -92
 CONF_DEFAULT_MAX_VOLUME: Final = -20
