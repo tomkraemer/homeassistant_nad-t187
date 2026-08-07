3950917284b62bf2f3bc1f6f26e69a89f9406f2a
tomkraemer <tom.dummie@web.de>
2026-08-07 14:25:05 +0200

    still zoneing

--- custom_components/nad/media_player.py
+++ custom_components/nad/media_player.py
@@ -61,7 +61,7 @@ class NAD(CoordinatorEntity, MediaPlayerEntity):
     _attr_name = None
     _attr_device_class = MediaPlayerDeviceClass.RECEIVER
 
-    zone = None
+    zone = ""
 
     def __init__(self, coordinator: NADReceiverCoordinator):
         """Initialize the NAD Receiver device."""
