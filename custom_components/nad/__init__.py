1fded9ae55638531af0f06cc006853d287fe2c36
tomkraemer <tom.dummie@web.de>
2026-08-08 15:26:55 +0200

    debug

--- custom_components/nad/__init__.py
+++ custom_components/nad/__init__.py
@@ -218,7 +218,7 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         """Fetch data from NAD Receiver."""
 
-        # _LOGGER.debug("TEST: Zone bekannt??: '%s'", self.zone)
+        _LOGGER.debug("TEST: Zone bekannt??: '%s'", self.zone)
 
 
         try:
