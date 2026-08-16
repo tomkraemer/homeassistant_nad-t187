202c7a84054264ad672120e6738c1c41f5830c94
tomkraemer <tom.dummie@web.de>
2026-08-16 12:23:19 +0200

    Power Initiation

--- custom_components/nad/__init__.py
+++ custom_components/nad/__init__.py
@@ -224,7 +224,7 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         """Fetch data from NAD Receiver."""
 
-        # _LOGGER.debug("TEST: Zone bekannt??: '%s'", self.zone)
+        _LOGGER.debug("XXXXXXXXXXXXXXXXXXX --------   TEST: Zone bekannt??: '%s'", self.zone)
 
 
         try:
